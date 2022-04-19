f154aa28449a000c97ce7626d9a69713f87402e0
EniyavanThangaraj <eniyavant405@gmail.com>
2022-04-19 15:10:44 +0530

    project structure

---
+++ src/org/scanner/Scannerll.java
@@ -0,0 +1,138 @@
+package org.scanner;
+
+import java.util.Scanner;
+
+public class Scannerll {
+
+public static void main(String[] args) {
+	
+	
+	Scanner s=new Scanner(System.in);
+	
+	System.out.println("enter the age ");
+	byte age=s.nextByte();
+	System.out.println("my age is"+age);
+	
+	System.out.println("enter the atm pin number ");
+	short atmpinno=s.nextShort();
+	System.out.println("my atm pin no is"+atmpinno);
+	
+	System.out.println("enter the pin code ");
+	int pincodeno =s.nextInt();
+	System.out.println("my pincode number is "+pincodeno);
+	
+	System.out.println("enter the salary ");
+	float salary=s.nextFloat();
+	System.out.println("my salary is "+salary);
+	
+     System.out.println("enter the result");	
+     boolean result=s.nextBoolean();
+     System.out.println("my result is "+result);
+	
+	System.out.println("enter the name ");
+	String name1 =s.next();
+
+			 System.out.println("my name is "+name1 );        		
+	
+	
+	System.out.println("enter the genter");
+	String nm=s.next();
+	char gender =nm.charAt(0);
+	System.out.println("my gender is "+gender);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
